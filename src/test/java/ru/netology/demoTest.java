5bb6e3b8e338d980833f5341b7949a8feb4be0a4
Диана Бережная <Lacshri@yandex.ru>
2022-07-14 22:29:48 +0300

    postman

--- src/test/java/ru/netology/demoTest.java
+++ src/test/java/ru/netology/demoTest.java
@@ -1,20 +1,26 @@
 package ru.netology;
 
+import io.restassured.response.ValidatableResponse;
+import io.restassured.specification.Argument;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
 import static io.restassured.RestAssured.given;
 
 public class demoTest {
 
     @Test
     void shouldDemoTest() {
-        given()
+        ValidatableResponse some_data = given()
                 .baseUri("https://postman-echo.com")
+                .header("Content-Type","application/json")
                 .body("some data")
                 .when()
                 .post("/post")
                 .then()
                 .statusCode(200)
-        ;
+                .header("content-length","425");
 
     }
 
